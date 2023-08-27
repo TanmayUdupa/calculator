b7de89bd0bdebf945c450275f74db87a675a2789
TanmayUdupa <tanmaypraveenudupa@gmail.com>
2023-08-27 12:58:54 +0530

    Make the calculator work and add error handling

--- script.js
+++ script.js
@@ -15,8 +15,9 @@ function divide(a, b) {
 }
 
 let first_number = 0;
-let last_number = 0;
+let last_number = 'a';
 let operator = '';
+let result = 0;
 
 function operate(op, a, b) {
     if (op === '+')
@@ -37,21 +38,135 @@ function operate(op, a, b) {
 
 let display_value = 0;
 
+let op_check = 0;
+let text_check = 0;
+let sol_obtained = 0;
+
 function display(x)
 {
     disp = document.querySelector(".display");
-    if (disp.textContent === "0")
+    if (disp.style.color === "red")
+    {
+        disp.style.color = "black";
+        disp.style.fontSize = "50px";
+    }
+
+    if (disp.textContent === "0" || (op_check === 1 && text_check === 1) || soln_obtained === 1)
     {
         disp.textContent = x;
+        text_check = 0;
+        soln_obtained = 0;
     }
     else
     {
         disp.textContent += x;
     }
     display_value = disp.textContent * 1;
-    console.log(display_value);
+    if (op_check === 0)
+    {
+        first_number = display_value;
+    }
+    else
+    {
+        last_number = display_value;
+    }
+}
+
+function oper(x)
+{
+    if (operator !== '')
+    {
+        if (last_number === 'a')
+        {
+            alert("Please enter a number for second operand");
+            return;
+        }
+        first_number = operate(operator, first_number, last_number);
+        last_number = 'a';
+        disp = document.querySelector(".display");
+        disp.textContent = first_number;
+    }
+    
+    if (x === '÷')
+    {
+        operator = '/';
+    }
+    else if (x === '×')
+    {
+        operator = '*';
+    }
+    else
+    {
+        operator = x;
+    }
+    op_check = 1;
+    text_check = 1;
+}
+
+function solve()
+{
+    if (last_number === 'a')
+    {
+        alert("Please enter operation before pressing '='");
+        return;
+    }
+    result = operate(operator, first_number, last_number);
+    text_check = 1;
+    if (result.toString().length > 8)
+    {
+        result = result.toExponential();
+    }
+    else if (!isFinite(result))
+    {
+        disp = document.querySelector(".display");
+        disp.textContent = "Division by zero error!!!";
+        disp.style.color = "red";
+        disp.style.fontSize = "34px";
+        soln_obtained = 1;
+        op_check = 0;
+        first_number = result;
+        text_check = 0;
+        last_number = 'a';
+        operator = '';
+        return;
+    }
+    display(result);
+    soln_obtained = 1;
+    op_check = 0;
+    first_number = result;
+    text_check = 0;
+    last_number = 'a';
+    operator = '';
+}
+
+function clear()
+{
+    disp = document.querySelector(".display");
+    if (disp.style.color === "red")
+    {
+        disp.style.color = "black";
+        disp.style.fontSize = "50px";
+    }
+    soln_obtained = 0;
+    op_check = 0;
+    first_number = 0;
+    text_check = 0;
+    last_number = 'a';
+    operator = '';
+    disp = document.querySelector(".display");
+    disp.textContent = 0;
 }
 
 num = document.querySelectorAll(".num");
 
 num.forEach((number) => (number.addEventListener('click', function () {display(number.textContent)})));
+
+op = document.querySelectorAll(".op");
+
+op.forEach((operator) => (operator.addEventListener('click', function() {oper(operator.textContent)})));
+
+sol = document.querySelector(".sol");
+sol.addEventListener('click', solve);
+
+clr = document.querySelector(".clear");
+clr.addEventListener('click', clear);
