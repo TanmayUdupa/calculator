e555aeb39a260483b82918881be7f02a4eb8fc00
TanmayUdupa <tanmaypraveenudupa@gmail.com>
2023-08-27 10:48:35 +0530

    Fill the display when number buttons are clicked

--- script.js
+++ script.js
@@ -34,3 +34,24 @@ function operate(op, a, b) {
 
     return divide(a, b);
 }
+
+let display_value = 0;
+
+function display(x)
+{
+    disp = document.querySelector(".display");
+    if (disp.textContent === "0")
+    {
+        disp.textContent = x;
+    }
+    else
+    {
+        disp.textContent += x;
+    }
+    display_value = disp.textContent * 1;
+    console.log(display_value);
+}
+
+num = document.querySelectorAll(".num");
+
+num.forEach((number) => (number.addEventListener('click', function () {display(number.textContent)})));
